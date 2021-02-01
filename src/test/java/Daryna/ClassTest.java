140bcd3795fa77664c8f98ef56b498533b624c68
darynatkm <65510511+darynatkm@users.noreply.github.com>
2021-02-01 21:43:07 +0100

    Daryna's Method

--- src/test/java/Daryna/ClassTest.java
+++ src/test/java/Daryna/ClassTest.java
@@ -2,4 +2,10 @@ package Daryna;
 
 public class ClassTest {
     //Daryna
+    public static void main(String[] args) {
+        add(2,4);
+    }
+    public static int add(int a,int b){
+        return a+b;
+    }
 }
