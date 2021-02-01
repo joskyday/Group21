1409a70bb8d3c17ff3ce60d95c368ecf6753f564
joskyday <joskyday@gmail.com>
2021-02-02 00:40:45 +0300

    last update with corrections

--- src/test/java/ArithmeticOperations.java
+++ src/test/java/ArithmeticOperations.java
@@ -1,9 +1,32 @@
 public class ArithmeticOperations {
     public static void main(String[] args) {
 
+      //Daryna
+
+        System.out.println(add(2, 4));
+
     }
     public static void Joseph(){
         //this is my method
     }
+  
 
+
+    public static int add(int a,int b){
+        return a+b;
+    }
+    public static void Emine(){
+        //this is my method
+    }
+
+    public static void Ali() {
+        System.out.println("Hi to everybody");
+    }
+    public static void Cansu(){
+        //this is my code
+    }
 }
+//try try try
+
+
+
